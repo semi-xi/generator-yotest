e6747a69b80615bfdf17405f40444286bf2820a5
建军 <lijianjun@yy.com>
2017-04-07 11:53:00 +0800

    更新创建目录的bug

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -6,7 +6,7 @@ var path = require('path'),
     win32 = process.platform === 'win32',
     filePath = process.env[win32 ? 'USERPROFILE' : 'HOME'],
     nodeModulePath = path.join(filePath, '.' + process.env.npm_package_name, 'node_modules'),
-    configPath = path.join(filePath,'.'+process.env.npm_package_name,'config.json');
+    configPath = path.join(filePath, '.' + process.env.npm_package_name, 'config.json');
 
 //不能跨2级创建目录
 // fs.mkdir(nodeModulePath,function(err){
@@ -16,16 +16,18 @@ var path = require('path'),
 //     console.log('node_modules目录创建完毕')
 // })
 //创建node_module
+//path cb
 mkdirp.sync(nodeModulePath, function(err) {
     if (err) {
         throw err;
     }
-    console.log('node_modules目录创建完毕')
 })
+console.log('node_modules目录创建完毕')
 //创建config.json
-mkdirp.sync(configPath,function(){
-    if(err){
+// path data config cb
+fs.writeFileSync(configPath, JSON.stringify({}, null, 4), {}, function(err) {
+    if (err) {
         throw err
     }
-    fs.writeFileSync(configPath,JSON.stringify({},null,4))
 })
+console.log('config.json创建完毕');
