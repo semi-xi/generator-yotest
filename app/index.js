5b2312744fa235baa076ce1bc4ce281a75fb52e4
建军 <lijianjun@yy.com>
2017-04-10 12:42:11 +0800

    兼容mac window的安装方式

--- app/index.js
+++ app/index.js
@@ -8,18 +8,10 @@ var generators = require('yeoman-generator'),
     del = require('del'),
     generatorName = 'gulp',
     win32 = process.platform === 'win32',
-    filePath = process.env[win32 ? 'USERPROFILE' : HOME],
+    filePath = process.env[win32 ? 'USERPROFILE' : 'HOME'],
     yeoname = require('../package.json').name,
     modulePath = path.join(filePath, '.' + yeoname, 'node_modules');
-//window  不兼容
-// function createNodeModules() {
-//     log('ccc')
-//     if (win32) {
-//         require('child_process').exec(`mklink /d .\\node_modules ${modulePath}`)
-//     } else {
-//         this.spawnCommand('ln', ['-s', modulePath, 'node_modules']);
-//     }
-// }
+
 module.exports = generators.Base.extend({
     constructor: function() {
         generators.Base.apply(this, arguments);
@@ -65,6 +57,19 @@ module.exports = generators.Base.extend({
                 name: 'projectName',
                 message: '输入项目名称',
                 default: 'test'
+            },
+            {
+                type: 'list',
+                name: 'projectInstall',
+                message: '模块安装方式',
+                choices: [{
+                    name: 'npm',
+                    value: 'npm',
+                    checked: true
+                }, {
+                    name: 'cnpm',
+                    value: 'cnpm(mac不可选)'
+                }]
             }
         ]
         return this.prompt(questions).then(
@@ -93,6 +98,7 @@ module.exports = generators.Base.extend({
     },
     end: function() {
         // del(['src/**/.gitignore','src/**/.npmignore','src/js/index.js']);
+        var self = this;
         var dirs = glob.sync('+(node_modules)');
         if (!_.includes(dirs, 'node_modules')) {
             // 创建软连接
@@ -103,40 +109,34 @@ module.exports = generators.Base.extend({
             }
             log(chalk.bold.green('软连接建立完成...'));
         }
-        //执行npm install 第1次会强制安装，后面的话会自动判断模块存在的话就不安装
-        //但是这样子会出现 网络不好就安装不了的情况
-        // this.installDependencies({
-        //     bower: false,
-        //     npm: true,
-        //     skipInstall: false,
-        //     callback: () => {
-        //         log(chalk.bold.green('软连接建立完成...'));
-        //         log(chalk.bold.green('工作流初始化完成...'));
-        //         log(chalk.bold.green('进入工作流...'));
-        //         this.spawnCommand('gulp');
-        //     }
-        // });
-        //执行cnpm 安装
-        var cp = require('child_process').exec('cnpm install');
-        // var cp = this.spawnCommand('cnpm install');
-        cp.on('close', (code) => {
-            log(chalk.bold.green('初始化完成...'));
-            log(chalk.bold.green('进入工作流...'));
-            // this.spawnCommand('gulp');
-        });
-        // log(chalk.bold.green('正在初始化...'));
-        // cp.stdout.on('data', (data) => {
-        //     log(`stdout: ${data}`)
-        // })
-        // cp.stderr.on('data', (data) => {
-        //     console.log(`stderr: ${data}`);
-        // })
-        // cp.on('close', (code) => {
-        //     log(new Date().getTime() - time.getTime())
-        //     log(chalk.bold.green('初始化完成...'));
-        //     log(chalk.bold.green('进入工作流...'));
-        //     // this.spawnCommand('gulp');
-        // });
+
+        switch (this.projectInstall) {
+            case 'npm':
+                //执行npm install
+                log('999')
+                self.installDependencies({
+                    bower: false,
+                    npm: true,
+                    skipInstall: false,
+                    callback: () => {
+                        log('安装完成')
+                        self.spawnCommand('gulp');
+                    }
+                });
+                log('10000')
+                break;
+            case 'cnpm':
+                if(!win32) break;
+                var cp = require('child_process').exec('cnpm install');
+                cp.stdout.on('data', (data) => {
+                    log(`stdout: ${data}`)
+                })
+                cp.on('close', (code) => {
+                    log('安装完成')
+                    self.spawnCommand('gulp');
+                });
+                break;
+        }
     },
 
 })
